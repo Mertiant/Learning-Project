470bd810d1e4640419c41899cb165e0d1defa8e4
Martin <Mlibusinesses@gmail.com>
2016-07-02 05:25:10 +0000

    Secret Message Project initiated

---
+++ Python/SecretMessage/Message.py
@@ -0,0 +1,10 @@
+'''
+File format: 028Araron.jpg
+
+Step 1: Go to Directory
+Step 2: Take file name as string.
+Step 3: Find only numbers in the string so(by finding individual chars)
+Step 4: Simply truncate the numbers.
+1) get file names
+2) for each file
+'''
